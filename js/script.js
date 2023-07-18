ff0dfd45677329ddf91ab401a7238e4221750d8a
maiullyDev <maiully.dev@gmail.com>
2023-07-18 17:16:41 -0300

    Salvando informações do formulário

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Open Modal
 const buttonNovaTransacao = document.querySelector("#button-novaTransacao");
 
 const buttonClose = document.querySelector(".button-close");
@@ -10,4 +11,47 @@ buttonNovaTransacao.addEventListener("click", () => {
 
 buttonClose.addEventListener("click", () => {
   modalNovaTransacao.style.display = "none";
+});
+
+// Saving form information
+const transactionForm = document.querySelector(".transactionForm");
+
+transactionForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(transactionForm);
+
+  const transactionObject = {};
+
+  for (let campo of formData) {
+    transactionObject[campo[0]] = campo[1];
+  }
+
+  console.log(transactionObject);
+  const tbody = document.querySelector("tbody");
+  tbody.insertAdjacentHTML(
+    "beforeend",
+    `<tr>
+    <td class="title">${transactionObject.title}</td>
+    <td>${transactionObject.tipe}</td>
+    <td>R$ ${transactionObject.value}</td>
+    <td>${transactionObject.data}</td>
+  </tr>`
+  );
+
+  //   for (let campo of formData) {
+  //     // 1. Criar um objeto que representa a transação:
+  //     // Adicionar a transação ao array de transações
+  //     // 3. Renderizar a transação na tabela
+  //     // insertAdjacentHTML
+  //   }
+  //   const tbody = document.querySelector("tbody");
+  //   tbody.insertAdjacentHTML(
+  //     "beforeend",
+  //     `<tr>
+  //   <td class="title">Salário de Desenvolvedor</td>
+  //   <td>Entrada</td>
+  //   <td>R$ 12.000,00</td>
+  //   <td>01/09/2022</td>
+  // </tr>`
+  //   );
 });
